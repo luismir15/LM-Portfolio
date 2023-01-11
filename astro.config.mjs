3b0cc61b6f63ad080cf1348cc17ab08899ddca80
Luis Miranda <luismir1513@gmail.com>
2023-01-11 16:34:04 -0500

    deployin site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://luismir15.github.io',
+    base: '/LM-Portfolio'
+}); 
